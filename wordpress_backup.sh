fedb6d266cc7a2dbde4908720cfa3b82502b83a5
Charl Potgieter <charl.j.potgieter@gmail.com>
2022-05-05 21:38:19 +1000

    Sundry

--- wordpress_backup.sh
+++ wordpress_backup.sh
@@ -11,9 +11,15 @@ source /etc/wordpress-backup.conf
 mkdir -p $backuppath/files
 mkdir -p $backuppath/database
 
+read -s -p "Enter ssh password for domain: " passw
 
-ssh $username@$domain
-source exit.sh   # exit ssh connection
+#ssh $username@$domain
+# source exit.sh   # exit ssh connection
+
+sshpass -p$passw ssh $username@$domain mysqldump -h localhost -u $username -p$passw --no-tablespaces i8640980_wp1 > ~/file_name_local.sql
+
+# May have to add host ip?
+# https://au.godaddy.com/help/connect-remotely-to-a-mysql-database-in-my-linux-hosting-account-16103
 
 
 # mysqldump -h localhost -u jkdwrlryv6s3 -p --no-tablespaces i8640980_wp1 > file_name.sql
