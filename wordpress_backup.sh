c0dc9b40d197c3e209d03f97359bd31378577ac0
Charl Potgieter <charl.j.potgieter@gmail.com>
2022-05-07 15:13:07 +1000

    sundry

--- wordpress_backup.sh
+++ wordpress_backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Requires installation of mysqldump and pv
+
 # Script should only be run as root
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "This script can only be run as root"
@@ -11,15 +13,13 @@ source /etc/wordpress-backup.conf
 mkdir -p $backuppath/files
 mkdir -p $backuppath/database
 
-read -s -p "Enter ssh password for domain: " passw
+# NOTE: For below to work first need to add pc (host) IP address to godaddy cpanel under Remote Mysql
+printf "\n"
+echo  "NOTE: For above to work first need to add pc (host) IP address to godaddy cpanel under Remote Mysql "
+echo "https://au.godaddy.com/help/connect-remotely-to-a-mysql-database-in-my-linux-hosting-account-16103 "
+printf "\n"
+echo "When prompted for a password, enter the mysql database password.  This can be located in file wp-config.php in the live site domain.  This file is located in public_html on Godaddy shared hosting."
+printf "\n"
+echo "Backing up database..."
 
-#ssh $username@$domain
-# source exit.sh   # exit ssh connection
-
-sshpass -p$passw ssh $username@$domain mysqldump -h localhost -u $username -p$passw --no-tablespaces i8640980_wp1 > ~/file_name_local.sql
-
-# May have to add host ip?
-# https://au.godaddy.com/help/connect-remotely-to-a-mysql-database-in-my-linux-hosting-account-16103
-
-
-# mysqldump -h localhost -u jkdwrlryv6s3 -p --no-tablespaces i8640980_wp1 > file_name.sql
+mysqldump --column-statistics=0 -h $domain -u $mysqlusername -p --no-tablespaces $mysqldatabase > $backuppath/database/mysql.database
