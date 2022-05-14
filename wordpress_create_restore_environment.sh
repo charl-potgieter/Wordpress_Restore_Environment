8686710ecac354757de926af127e51bcafc7140e
Charl Potgieter <charl.j.potgieter@gmail.com>
2022-05-14 17:07:13 +1000

    Sundry

--- wordpress_create_restore_environment.sh
+++ wordpress_create_restore_environment.sh
@@ -14,124 +14,122 @@ loginuserhome=$( getent passwd $loginuser | cut -d: -f6 )
 configfilepath=$loginuserhome/.wordpress-backup.conf
 
 source $configfilepath
-echo $domain
-echo $pausetocheck
 
-# printf "\n"
-# echo "Updating system...................."
-# printf "\n"
-# sleep 1
-# apt-get update
-# apt-get upgrade
+printf "\n"
+echo "Updating system...................."
+printf "\n"
+sleep 1
+apt-get update
+apt-get upgrade
 
-# printf "\n"
-# echo "Install all necessary programs...................."
-# printf "\n"
-# sleep 1
-# apt-get install \
-#     apache2 \
-#     ghostscript \
-#     libapache2-mod-php \
-#     mysql-server \
-#     php \
-#     php-bcmath \
-#     php-curl \
-#     php-imagick \
-#     php-intl \
-#     php-json \
-#     php-mbstring \
-#     php-mysql \
-#     php-xml \
-#     php-zip \
-#     dos2unix
+printf "\n"
+echo "Install all necessary programs...................."
+printf "\n"
+sleep 1
+apt-get install \
+    apache2 \
+    ghostscript \
+    libapache2-mod-php \
+    mysql-server \
+    php \
+    php-bcmath \
+    php-curl \
+    php-imagick \
+    php-intl \
+    php-json \
+    php-mbstring \
+    php-mysql \
+    php-xml \
+    php-zip \
+    dos2unix
 
 
-# service apache2 start
+service apache2 start
 
-# if [ $pausetocheck -eq 1 ]
-# then
-#     printf "\n"
-#     echo "-------------------------------"
-#     echo "      SYSTEM STATUS CHECK"
-#     echo "-------------------------------"
-#     echo "To check status enter localhost in browser, preferably in private mode.  "
-#     echo "Apache Ubuntu default page should be displayed.  "
-#     printf "\n"
-#     read -p "Press any key to continue... " -n1 -s
-#     printf "\n\n"
-# fi
-
-
-# printf "\n"
-# echo "Creating Wordpress directory...................."
-# printf "\n"
-# sleep 1
-
-# mkdir -p /srv/www
-# chown www-data: /srv/www
-# sudo -u www-data mkdir -p /srv/www/wordpress
+if [ $pausetocheck -eq 1 ]
+then
+    printf "\n"
+    echo "-------------------------------"
+    echo "      SYSTEM STATUS CHECK"
+    echo "-------------------------------"
+    echo "To check status enter localhost in browser, preferably in private mode.  "
+    echo "Apache Ubuntu default page should be displayed.  "
+    printf "\n"
+    read -p "Press any key to continue... " -n1 -s
+    printf "\n\n"
+fi
 
 
-# printf "\n"
-# echo "Creating apache configuration for Wordpress...................."
-# printf "\n"
-# sleep 1
+printf "\n"
+echo "Creating Wordpress directory...................."
+printf "\n"
+sleep 1
 
-# conf_file_path=/etc/apache2/sites-available/wordpress.conf
-# echo "<VirtualHost *:80>
-#     DocumentRoot /srv/www/wordpress
-#     <Directory /srv/www/wordpress>
-#         Options FollowSymLinks
-#         AllowOverride Limit Options FileInfo
-#         DirectoryIndex index.php
-#         Require all granted
-#     </Directory>
-#     <Directory /srv/www/wordpress/wp-content>
-#         Options FollowSymLinks
-#         Require all granted
-#     </Directory>
-# </VirtualHost>" > $conf_file_path
-
-# #Enable site
-# a2ensite wordpress
-
-# #Enable URL rewriting
-# a2enmod rewrite
-
-# # Disable the default "It Works” site
-# a2dissite 000-default
-
-# service apache2 start
-# service apache2 reload
+mkdir -p /srv/www
+chown www-data: /srv/www
+sudo -u www-data mkdir -p /srv/www/wordpress
 
 
-# printf "\n"
-# echo "Configure MYSQL database...................."
-# printf "\n"
-# sleep 1
+printf "\n"
+echo "Creating apache configuration for Wordpress...................."
+printf "\n"
+sleep 1
+
+conf_file_path=/etc/apache2/sites-available/wordpress.conf
+echo "<VirtualHost *:80>
+    DocumentRoot /srv/www/wordpress
+    <Directory /srv/www/wordpress>
+        Options FollowSymLinks
+        AllowOverride Limit Options FileInfo
+        DirectoryIndex index.php
+        Require all granted
+    </Directory>
+    <Directory /srv/www/wordpress/wp-content>
+        Options FollowSymLinks
+        Require all granted
+    </Directory>
+</VirtualHost>" > $conf_file_path
+
+#Enable site
+a2ensite wordpress
+
+#Enable URL rewriting
+a2enmod rewrite
+
+# Disable the default "It Works” site
+a2dissite 000-default
+
+service apache2 start
+service apache2 reload
 
 
-# service mysql start
-
-# mysql -e "CREATE DATABASE wordpress;"
-
-# mysql -e "CREATE USER wordpress@localhost IDENTIFIED BY '${wordpress_password}';"
-# mysql -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost;"
-# mysql -e "FLUSH PRIVILEGES;"
-# service mysql restart
+printf "\n"
+echo "Configure MYSQL database...................."
+printf "\n"
+sleep 1
 
 
-# printf "\n"
-# echo "Install phpMyAdmin...................."
-# echo "Select apache2 as webserver"
-# echo "Configure database for phpMyAdmin with dbconfig-common (dont perform manual configuration)"
-# printf "\n"
-# read -p "Press any key to continue... " -n1 -s
+service mysql start
 
-# apt-get install phpmyadmin
+mysql -e "CREATE DATABASE wordpress;"
 
-# service mysql restart
-# service apache2 restart
+mysql -e "CREATE USER wordpress@localhost IDENTIFIED BY '${passwordwp}';"
+mysql -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost;"
+mysql -e "FLUSH PRIVILEGES;"
+service mysql restart
+
+
+printf "\n"
+echo "Install phpMyAdmin...................."
+echo "Select apache2 as webserver"
+echo "Configure database for phpMyAdmin with dbconfig-common (dont perform manual configuration)"
+printf "\n"
+read -p "Press any key to continue... " -n1 -s
+
+apt-get install phpmyadmin
+
+service mysql restart
+service apache2 restart
 
 
 if [ $pausetocheck -eq 1 ]
